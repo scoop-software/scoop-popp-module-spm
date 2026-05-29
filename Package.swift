f73f529a1bc6a73500cb7aa95eb0cb13ea02c0a8
Philipp Kursawe <philipp.kursawe@scoop-software.de>
2026-05-29 16:27:49 +0200

    feat: bump SPM binary target to 0.18.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScoopPopp",
-            url: "https://github.com/scoop-software/scoop-popp-module-spm/releases/download/PLACEHOLDER/ScoopPopp.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            url: "https://github.com/scoop-software/scoop-popp-module-spm/releases/download/v0.18.0/ScoopPopp.xcframework.zip",
+            checksum: "bb0cce9fb97b7307f97fa650b50db9cf3ebca0abcede504890459278ee2fc786"
         )
     ]
 )
